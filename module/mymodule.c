9d600fae632a22af26a66d4ddc51ab8779be85bf
Sinemis Toktas <stoktas20@ku.edu.tr>
2025-04-27 18:13:26 +0000

    init varibales

--- module/mymodule.c
+++ module/mymodule.c
@@ -5,29 +5,30 @@
 #include <linux/pid.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/fdtable.h>
 
 // Meta Information
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sinemis & Yamaç");
 MODULE_DESCRIPTION("COMP 304 SPRING 2025 PROJECT 1: lsfd Kernel Module");
 
+#define BUFFER_SIZE 8192 // max size of output buffer
 
-// A function that runs when the module is first loaded
-int simple_init(void) {
-	struct task_struct *ts;
+static struct proc_dir_entry *proc_entry; // pointer to /proc/lsfd
+static char *output_buffer;
+static size_t output_size; // current size of output buffer
+static pid_t target_pid = -1; // default PID -1
 
-	ts = get_pid_task(find_get_pid(4), PIDTYPE_PID);
+// Forward declarations
+static int simple_init(void);
+static void simple_exit(void);
+static ssize_t lsfd_read(struct file *file, char __user *ubuf, size_t count, loff_t *ppos);
+static ssize_t lsfd_write(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos);
 
-	printk("Hello from the kernel");
-	printk("command: %s\n", ts->comm);
-	return 0;
-}
 
-
-// A function that runs when the module is removed
-void simple_exit(void) {
-	printk("Goodbye from the kernel");
-}
 
 
 module_init(simple_init);
